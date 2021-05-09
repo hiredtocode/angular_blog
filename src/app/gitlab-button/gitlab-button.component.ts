9778d81a16993f203a52d04ace13c9f7296ffcb6
Jason <jasonhan@kakao.com>
2021-05-09 19:58:34 +0900

    Gitlab + Github button components

---
+++ src/app/gitlab-button/gitlab-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gitlab-button',
+  templateUrl: './gitlab-button.component.html',
+  styleUrls: ['./gitlab-button.component.sass']
+})
+export class GitlabButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
