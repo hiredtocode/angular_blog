e26ae4436e92c1867e8606338aacb9f5b50b7a37
hiredtocode <jasonhan@kakao.com>
2021-05-19 20:07:37 +0900

    Re-done the top navbar without angular material

---
+++ src/app/top-navbar/top-navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-top-navbar',
+  templateUrl: './top-navbar.component.html',
+  styleUrls: ['./top-navbar.component.css'],
+})
+export class NavigationComponent {
+  links: string[] = ['main', 'resume', 'portfolio'];
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
