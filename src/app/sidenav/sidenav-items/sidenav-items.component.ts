550fd18ee42bb2da931de74bc58b45fe6dc97ede
hiredtocode <jasonhan@kakao.com>
2021-05-19 20:05:11 +0900

    sidenav-items updated with style

---
+++ src/app/sidenav/sidenav-items/sidenav-items.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-items',
+  templateUrl: './sidenav-items.component.html',
+  styleUrls: ['./sidenav-items.component.css'],
+})
+export class ComponentNavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
